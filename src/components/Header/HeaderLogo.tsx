f242fa981abe13b8fbbc807a636079c35d80f6fa
Hassan-ureshi <hassanqureshi8886@gmail.com>
2025-11-19 08:17:15 -0800

    add animated header logo component

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -1,17 +1,18 @@
 import { motion } from "framer-motion";
+import logo from '../../assets/logo.png';
 
 const HeaderLogo = () => {
   return (
-    <div className="flex items-center">
-  <motion.h1
-  initial={{ opacity: 0, y: -10 }}
-  animate={{ opacity: 1, y: 0 }}
-  transition={{ duration: 0.6 }}
-  className="text-2xl md:text-3xl font-bold pl-4 "
->
-  Amina Gulzar
-</motion.h1>
-
+    <div className="flex items-center gap-4">
+      <img src={logo} alt="logo" className="rounded-full h-15 w-15 md:h-20 md:w-20" />
+      <motion.h1
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="text-2xl md:text-3xl font-bold"
+      >
+        Amina Gulzar
+      </motion.h1>
     </div>
   );
 };
