89358d68f53873f7215bda6fe917c2ec0d8eb0e3
Hassan-ureshi <hassanqureshi8886@gmail.com>
2025-11-26 02:33:33 -0800

    fix: remove unused icon imports in navItems

--- src/components/Header/navItems.ts
+++ src/components/Header/navItems.ts
@@ -1,7 +1,7 @@
-  import { FaHome, FaUser, FaEnvelope, FaBriefcase } from "react-icons/fa";
+import { FaHome, FaUser, FaEnvelope } from "react-icons/fa";
 
-  export const navItems = [
-    { label: "Home", icon: FaHome, href: "/" },
-    { label: "About", icon: FaUser, href: "/about" },
-    { label: "Contact", icon: FaEnvelope, href: "/contact" },
-  ];
+export const navItems = [
+  { label: "Home", icon: FaHome, href: "/" },
+  { label: "About", icon: FaUser, href: "/about" },
+  { label: "Contact", icon: FaEnvelope, href: "/contact" },
+];
